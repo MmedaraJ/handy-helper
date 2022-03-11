ca11f98b1caf5ee270e5df0d80c52edcfcfcad1d
Mmedara <--replace-all>
2022-03-11 13:36:06 -0500

    Initial Commit

---
+++ handy_helper/urls.py
@@ -0,0 +1,27 @@
+"""handy_helper URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include, handler400, handler403, handler404, handler500
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('apps.jobs.urls', namespace='jobs'))
+]
+
+handler404 = 'jobs.views.error_404'
+handler500 = 'jobs.views.error_500'
+handler403 = 'jobs.views.error_403'
+handler400 = 'jobs.views.error_400'
